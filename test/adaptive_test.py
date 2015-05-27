700da13b45b21036ff5cbe36947cf1f8eb39a18b
Leif Johnson <leif@lmjohns3.com>
2015-05-27 16:46:29 -0500

    Fix test failures by reducing learning rate.

--- test/adaptive_test.py
+++ test/adaptive_test.py
@@ -3,10 +3,10 @@ import util
 
 class TestESGD:
     def test_rosen(self):
-        util.assert_progress(*util.build_rosen('esgd'))
+        util.assert_progress(*util.build_rosen('esgd'), learning_rate=1e-6)
 
     def test_factor(self):
-        util.assert_progress(*util.build_factor('esgd'))
+        util.assert_progress(*util.build_factor('esgd'), learning_rate=1e-6)
 
 
 class TestRProp:
