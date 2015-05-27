dafbe424546020cc5a53eae5d10391d3dbf81870
Leif Johnson <leif@lmjohns3.com>
2015-05-26 20:42:41 -0500

    Update style test to work with this package.

--- test/style_test.py
+++ test/style_test.py
@@ -6,8 +6,8 @@ import pep8
 class TestCodeFormat:
     def test_pep8(self):
         def match(*p):
-            s = ['theanets'] + list(p) + ['*.py']
+            s = ['downhill'] + list(p) + ['*.py']
             return glob.glob(os.path.join(*s))
         pep8style = pep8.StyleGuide(config_file='setup.cfg')
-        result = pep8style.check_files(match() + match('layers'))
+        result = pep8style.check_files(match())
         assert result.total_errors == 0
