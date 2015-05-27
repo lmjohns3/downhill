e66b9ed7dd8bb44688a115297405f528ccc4ad42
Leif Johnson <leif@lmjohns3.com>
2015-05-26 21:15:05 -0500

    Change interface for test helpers.

--- test/base_test.py
+++ test/base_test.py
@@ -5,20 +5,23 @@ import util
 
 class TestBuild:
     def test_sgd(self):
-        assert isinstance(util.build_rosen('sgd'), downhill.SGD)
-        assert isinstance(util.build_factor('sgd'), downhill.SGD)
+        assert isinstance(util.build_rosen('sgd')[0], downhill.SGD)
+        assert isinstance(util.build_factor('sgd')[0], downhill.SGD)
 
     def test_nag(self):
-        assert isinstance(util.build_rosen('nag'), downhill.NAG)
+        assert isinstance(util.build_rosen('nag')[0], downhill.NAG)
+
+    def test_rprop(self):
+        assert isinstance(util.build_rosen('RProp')[0], downhill.RProp)
 
     def test_rmsprop(self):
-        assert isinstance(util.build_rosen('RmsProp'), downhill.RMSProp)
+        assert isinstance(util.build_rosen('RmsProp')[0], downhill.RMSProp)
 
     def test_adadelta(self):
-        assert isinstance(util.build_rosen('ADADELTA'), downhill.ADADELTA)
+        assert isinstance(util.build_rosen('ADADELTA')[0], downhill.ADADELTA)
 
     def test_esgd(self):
-        assert isinstance(util.build_rosen('EsGd'), downhill.ESGD)
+        assert isinstance(util.build_rosen('EsGd')[0], downhill.ESGD)
 
 
 class Tester(downhill.Optimizer):
@@ -27,14 +30,28 @@ class Tester(downhill.Optimizer):
 
 
 class TestOptimizer:
-    def test_tester(self):
-        opt = util.build_rosen('tester')
+    def test_rosen(self):
+        opt, train = util.build_rosen('tester')
         assert isinstance(opt, Tester)
 
         # run the optimizer for three iterations. check that the x and y values
         # (being monitored) increase at each iteration.
-        for i, (tm, vm) in enumerate(opt.iteropt([[]])):
+        for i, (tm, vm) in enumerate(opt.iteropt(train)):
             assert tm['x'] >= vm['x']
             assert tm['y'] >= vm['y']
+            if i == 2:
+                break
+
+    def test_factor(self):
+        opt, train = util.build_factor('tester')
+        assert isinstance(opt, Tester)
+
+        # run the optimizer for two iterations. check that the u and v values
+        # (being monitored) are reasonable at the start.
+        for i, (tm, vm) in enumerate(opt.iteropt(train)):
+            assert vm['u<1'] == 0.001
+            assert vm['u<-1'] == 0
+            assert vm['v<1'] == 1
+            assert vm['v<-1'] == 0
             if i == 2:
                 break
--- test/util.py
+++ test/util.py
@@ -12,21 +12,30 @@ def build_rosen(algo):
         params=[x],
         inputs=[],
         updates=(),
-        monitors=[('x', x[:-1].sum()), ('y', x[1:].sum())])
+        monitors=[('x', x[:-1].sum()), ('y', x[1:].sum())]), [[]]
 
 
 def build_factor(algo):
     x = TT.matrix('x')
-    u = theano.shared(0.001 + np.zeros((100, 10), 'f'), name='u')
-    v = theano.shared(-0.001 + np.zeros((10, 100), 'f'), name='v')
+    u = theano.shared(np.arange(1000).reshape((100, 10)).astype('f'), name='u')
+    v = theano.shared(0.2 + np.zeros((10, 100), 'f'), name='v')
     return downhill.build(
         algo,
-        loss=TT.mean(x - TT.dot(u, v)),
+        loss=TT.sum(TT.sqr(x - TT.dot(u, v))),
         params=[u, v],
         inputs=[x],
         monitors=[
             ('u<1', (u < 1).mean()),
             ('u<-1', (u < -1).mean()),
-            ('v<1', (u < 1).mean()),
-            ('v<-1', (u < -1).mean()),
-        ])
+            ('v<1', (v < 1).mean()),
+            ('v<-1', (v < -1).mean()),
+        ]), [np.dot(np.arange(1000).reshape((100, 10)).astype('f'),
+                    0.1 + np.zeros((10, 100), 'f'))[None, :, :]]
+
+
+def assert_progress(opt, train, valid=None, **kwargs):
+    mover = opt.iteropt(train, valid=valid, **kwargs)
+    train0, valid0 = next(mover)
+    train1, valid1 = next(mover)
+    assert train1['loss'] < valid0['loss']   # should have made progress!
+    assert valid1['loss'] == valid0['loss']  # no new validation occurred
