85f5b50b13332693d164ec0819f05a222bf4e92b
Leif Johnson <leif@lmjohns3.com>
2015-05-29 01:11:13 -0500

    Add more optimizers, change initial conditions.

--- examples/rosenbrock.py
+++ examples/rosenbrock.py
@@ -13,37 +13,46 @@ climate.enable_default_logging()
 _, ax = plt.subplots(1, 1)
 
 # run several optimizers for comparison.
-for i, (algo, label, kw) in enumerate((
-        ('sgd', 'SGD - Momentum 0', {}),
-        ('sgd', 'SGD - Momentum 0.5', dict(momentum=0.5, nesterov=False)),
-        ('rmsprop', 'RMSProp - Momentum 0', {}),
-        ('rmsprop', 'RMSProp - Momentum 0.5', dict(momentum=0.5, nesterov=False)),
-        ('adam', 'Adam - Momentum 0', {}),
-        #('esgd', 'ESGD - Momentum 0', {}),
-        ('rprop', 'RProp - Momentum 0', {}),
-        ('adadelta', 'ADADELTA - Momentum 0', {}),
+for i, (algo, label, m, n, kw) in enumerate((
+        ('sgd', 'SGD - Momentum 0', 0, False, {}),
+        ('sgd', 'SGD - Momentum 0.1', 0.1, False, {}),
+        ('sgd', 'SGD - Momentum 0.5', 0.5, False, {}),
+        ('sgd', 'SGD - Nesterov 0.5', 0.5, True, {}),
+        ('rmsprop', 'RMSProp - Momentum 0', 0, False, {}),
+        ('rmsprop', 'RMSProp - Momentum 0.5', 0.5, False, {}),
+        ('adam', 'Adam - Momentum 0', 0, False, {}),
+        ('adam', 'Adam - Momentum 0.5', 0.5, False, {}),
+        #('esgd', 'ESGD - Momentum 0', dict(learning_rate=0.02)),
+        ('rprop', 'RProp - Momentum 0', 0, False, {}),
+        ('rprop', 'RProp - Momentum 0.5', 0.5, False, {}),
+        ('adadelta', 'ADADELTA - Momentum 0', 0, False,
+         dict(rms_halflife=10, rms_regularizer=1e-2)),
+        ('adadelta', 'ADADELTA - Momentum 0.5', 0.5, False,
+         dict(rms_halflife=10, rms_regularizer=1e-2)),
         )):
     print(label)
-    x = theano.shared(np.array([-1.1, -0.4], 'f'), name='x')
+    x = theano.shared(np.array([-1.1, -0.4 + 0.03 * i], 'f'), name='x')
     opt = downhill.build(
         algo,
         loss=(100 * (x[1:] - x[:-1] ** 2) ** 2 + (1 - x[:-1]) ** 2).sum(),
         params=[x],
         inputs=[],
-        monitors=[('x', x[:-1].sum()), ('y', x[1:].sum())])
+        monitors=[('x', x[:-1].sum()), ('y', x[1:].sum())],
+        monitor_gradients=True)
     xs, ys = [], []
     for tm, _ in opt.iteropt([[]],
-                             max_gradient_clip=1,
+                             learning_rate=0.02,
+                             momentum=m,
+                             nesterov=n,
+                             max_gradient_clip=10,
                              min_improvement=0,
-                             learning_rate=0.01,
                              patience=0,
                              **kw):
         xs.append(tm['x'])
         ys.append(tm['y'])
         if len(xs) == 100:
             break
-    ax.plot(np.array(xs), np.array(ys) + 0.05 * i,
-            'o-', label=label, alpha=0.3)
+    ax.plot(xs, ys, 'o-', label=label, alpha=0.3)
 
 # make a contour plot of the rosenbrock function surface.
 a = b = np.arange(-1.2, 1.2, 0.05)
