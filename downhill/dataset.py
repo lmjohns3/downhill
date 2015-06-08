a107963f59c6fdc70fd6a42d62dbf814823526a4
Leif Johnson <leif@lmjohns3.com>
2015-06-08 12:42:50 -0500

    Catch AttributeError for no length.
    Closes issue #1.

--- downhill/dataset.py
+++ downhill/dataset.py
@@ -100,7 +100,7 @@ class Dataset:
         if not self.iteration_size:
             try:
                 self.iteration_size = len(inputs)
-            except TypeError:  # has no len
+            except TypeError, AttributeError:  # has no len
                 self.iteration_size = 100
         logging.info('%s: %d mini-batches from callable',
                      self.name, self.iteration_size)
