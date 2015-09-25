f925a399796f6125a8339c535d28cda660a37d40
Leif Johnson <leif@lmjohns3.com>
2015-09-25 10:56:26 -0500

    Fix a couple PEP8 things. [ci skip]

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setuptools.setup(
     author='Leif Johnson',
     author_email='leif@lmjohns3.com',
     description='Stochastic optimization routines for Theano',
-    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.rst')).read(),
+    long_description=open(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'README.rst')).read(),
     license='MIT',
     url='http://github.com/lmjohns3/downhill',
     keywords=('adadelta '
