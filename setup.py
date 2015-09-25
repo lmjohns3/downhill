7fe01d3b9c9903abe7664563af4bbd1e8c9d1dc1
Leif Johnson <leif@lmjohns3.com>
2015-09-25 11:37:22 -0500

    Bump version on master. [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='downhill',
-    version='0.3.0',
+    version='0.4.0pre',
     packages=setuptools.find_packages(),
     author='Leif Johnson',
     author_email='leif@lmjohns3.com',
